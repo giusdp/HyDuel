16633c000c7318cf65cd20271310137666022280
Giuseppe De Palma <dpl.gsp@tutanota.com>
2026-01-20 21:14:26 +0100

    refactor CardAssetStore to extend HytaleAssetStore

--- src/main/java/com/giusdp/htduels/asset/CardAssetStore.java
+++ src/main/java/com/giusdp/htduels/asset/CardAssetStore.java
@@ -1,47 +1,48 @@
 package com.giusdp.htduels.asset;
 
-import com.hypixel.hytale.assetstore.AssetStore;
-import com.hypixel.hytale.assetstore.AssetUpdateQuery;
+import com.hypixel.hytale.assetstore.codec.AssetCodec;
 import com.hypixel.hytale.assetstore.map.DefaultAssetMap;
-import com.hypixel.hytale.event.IEventBus;
-import com.hypixel.hytale.server.core.HytaleServer;
+import com.hypixel.hytale.server.core.asset.HytaleAssetStore;
 
 import javax.annotation.Nonnull;
-import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
+import java.util.function.Function;
 
-public class CardAssetStore extends AssetStore<String, CardAsset, DefaultAssetMap<String, CardAsset>> {
+public class CardAssetStore extends HytaleAssetStore<String, CardAsset, DefaultAssetMap<String, CardAsset>> {
     public CardAssetStore(Builder builder) {
         super(builder);
-    }
-
-    @Override
-    protected IEventBus getEventBus() {
-        return HytaleServer.get().getEventBus();
-    }
-
-    @Override
-    public void addFileMonitor(@Nonnull String packKey, Path path) {
-    }
-
-    @Override
-    public void removeFileMonitor(Path path) {
-    }
-
-    @Override
-    protected void handleRemoveOrUpdate(Set<String> removed, Map<String, CardAsset> updated, @Nonnull AssetUpdateQuery query) {
     }
 
     public static Builder builder() {
         return new Builder();
     }
 
-    public static class Builder extends AssetStore.Builder<String, CardAsset, DefaultAssetMap<String, CardAsset>, Builder> {
+    public static class Builder extends HytaleAssetStore.Builder<String, CardAsset, DefaultAssetMap<String, CardAsset>> {
         public Builder() {
             super(String.class, CardAsset.class, new DefaultAssetMap<>());
         }
 
+        @Nonnull
+        @Override
+        public Builder setPath(@Nonnull String path) {
+            super.setPath(path);
+            return this;
+        }
+
+        @Nonnull
+        @Override
+        public Builder setCodec(@Nonnull AssetCodec<String, CardAsset> codec) {
+            super.setCodec(codec);
+            return this;
+        }
+
+        @Nonnull
+        @Override
+        public Builder setKeyFunction(@Nonnull Function<CardAsset, String> keyFunction) {
+            super.setKeyFunction(keyFunction);
+            return this;
+        }
+
+        @Nonnull
         @Override
         public CardAssetStore build() {
             return new CardAssetStore(this);
