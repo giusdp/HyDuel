82b79b16995a3ef2992bdadcd1af03a78813fa3b
giusdp <depalma.gsp@gmail.com>
2026-01-26 14:54:41 +0100

    delete SpawnCardCommand.java

--- src/main/java/com/giusdp/htduels/DuelsPlugin.java
+++ src/main/java/com/giusdp/htduels/DuelsPlugin.java
@@ -4,7 +4,6 @@ import com.giusdp.htduels.asset.CardAsset;
 import com.giusdp.htduels.asset.CardAssetCodec;
 import com.giusdp.htduels.asset.CardAssetStore;
 import com.giusdp.htduels.command.ResetCameraCommand;
-import com.giusdp.htduels.command.SpawnCardCommand;
 import com.giusdp.htduels.command.duel.DuelCommand;
 import com.giusdp.htduels.component.BoardLayoutComponent;
 import com.giusdp.htduels.component.CardComponent;
@@ -68,7 +67,6 @@ public class DuelsPlugin extends JavaPlugin {
         this.getCommandRegistry().registerCommand(new DuelCommand());
 
         this.getCommandRegistry().registerCommand(new ResetCameraCommand());
-        this.getCommandRegistry().registerCommand(new SpawnCardCommand());
     }
 
     private void setupInteractions() {
